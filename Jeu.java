8c18e5ba1544f6e1f88b387de89a33e86b33ec6e
Elijah-ru <44814098+Elijah-ru@users.noreply.github.com>
2019-04-16 12:08:25 +0200

    ajout fonctions structurelles

--- Jeu.java
+++ Jeu.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atomx;
-
+package vsimple;
+import java.util.Scanner;
 
 /**
  *
@@ -13,73 +13,94 @@ package atomx;
 public class Jeu {
     private boolean fini;
     private Joueur joueur;
+    private LesCases tab;
 
-    public void setFini(boolean fini) {
-        this.fini = fini;
-    }
-    public void setJoueur(Joueur joueur) {
-        this.joueur = joueur;
-    }
     public boolean isFini() {
         return fini;
     }
     public Joueur getJoueur() {
         return joueur;
     }
-    
-    public Jeu(String pseudo, int taille){//initialise le plateau de jeu et les instances necessaire au jeu
-        setJoueur(new Joueur(pseudo));
-        
+    public LesCases getTab() {
+        return tab;
+    }
+    public void setFini(boolean fini) {
+        this.fini = fini;
+    }
+    public void setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    public void setTab(LesCases tab) {
+        this.tab = tab;
+    }
+    public void getFini(){
+        boolean tourfini=false;
+        if (getTab().getNbObstacles()==0||getJoueur().getSolde()<=0)
+            tourfini=true;
+        setFini(tourfini);
         }
-    public void creaCotes(int taille){
-        taille=10;//pour le moment
-        Ref.setRef(new Ref());
-        int[] pos=new int[taille-1];
-        pos[0]=2; pos[1]=7; pos[2]=1; pos[3]=3; pos[4]=6; pos[5]=8; pos[6]=0; pos[7]=4; pos[8]=9;
-        Ref.getRef().setxNul(new CaseVide(new CaseNonVide(new Position(0, (int)Math.floor(taille/2)))));
-        Ref.getRef().setxMax(new CaseVide(new CaseNonVide(new Position(taille-1, (int)Math.floor(taille/2)))));
-        Ref.getRef().setyNul(new CaseVide(new CaseNonVide(new Position((int)Math.floor(taille/2), 0))));
-        Ref.getRef().setyMax(new CaseVide(new CaseNonVide(new Position((int)Math.floor(taille/2), taille-1))));
-        for(int i=0; i<taille; i++){
-            CaseVide c=new CaseVide(new CaseNonVide(new Position(0, pos[i])));
-            CaseVide act=Ref.getRef().getxNul();//pointe vers la premiere case qu'on a placé
-            System.out.println(act.getfMoins());
-                while(act.getfMoins()==null&&act.getfPlus()==null){/*on continue jusqu'a ce que la case actuelle soit une extremité*/
-                    if(c.getLien().getPos().estPlusGrandeQue(act.getLien().getPos())){
-                        act=act.getfPlus();
-                        }
-                    else
-                        act=act.getfMoins();
-                    }
-            c=new CaseVide(new CaseNonVide(new Position(taille-1, pos[i])));
-            act=Ref.getRef().getxNul();//pointe vers la premiere case qu'on a placé
-                while(act.getfMoins()==null&&act.getfPlus()==null){/*on continue jusqu'a ce que la case actuelle soit une extremité*/
-                    if(c.getLien().getPos().estPlusGrandeQue(act.getLien().getPos())){
-                        act=act.getfPlus();
-                        }
-                    else
-                        act=act.getfMoins();
-                    }
-            c=new CaseVide(new CaseNonVide(new Position(0, pos[i])));
-            act=Ref.getRef().getxNul();//pointe vers la premiere case qu'on a placé
-                while(act.getfMoins()==null&&act.getfPlus()==null){/*on continue jusqu'a ce que la case actuelle soit une extremité*/
-                    if(c.getLien().getPos().estPlusGrandeQue(act.getLien().getPos())){
-                        act=act.getfPlus();
-                        }
-                    else
-                        act=act.getfMoins();
-                    }
-            c=new CaseVide(new CaseNonVide(new Position(0, pos[i])));
-            act=Ref.getRef().getxNul();//pointe vers la premiere case qu'on a placé
-                while(act.getfMoins()==null&&act.getfPlus()==null){/*on continue jusqu'a ce que la case actuelle soit une extremité*/
-                    if(c.getLien().getPos().estPlusGrandeQue(act.getLien().getPos())){
-                        act=act.getfPlus();
-                        }
-                    else
-                        act=act.getfMoins();
-                    }       
+    public void tour(){
+        Scanner input=new Scanner(System.in);
+        Position p;
+        getTab().getCases()[5][5]=new Prison(new Position(5,5));
+        do{
+        System.out.println("Entrez l'endroit où vous souhaitez faire commencez votre particule.");
+        int x=input.nextInt();
+        int y=input.nextInt();
+        p=new Position(x, y);
+        }while(!p.isValide(getTab().getCases().length));
+        System.out.println("Entrez le poid que vous souhaitez attribuer à la particule :");
+        int poid=input.nextInt();
+        getJoueur().setParticule(new Particule(p, getDirInitiale(p), poid));
+        getJoueur().setSolde(getJoueur().getSolde()-poid);
+        do{
+            avance();
+            getJoueur().getParticule().arret(getTab().getCases().length);
+        }while(getJoueur().getParticule().isActive());
+        int x=getJoueur().getParticule().getPos().getX(), y=getJoueur().getParticule().getPos().getY();
+        if(x==-1 || x==getTab().getCases().length || y==-1 || y==getTab().getCases().length){
+            System.out.println("Vous avez récupéré votre particule avec un poid de "+ getJoueur().getParticule().getPoid()+ " sur la case "+getJoueur().getParticule().getPos());
+            getJoueur().setSolde(getJoueur().getSolde()+getJoueur().getParticule().getPoid());
             }
-
+        else{
+            System.out.println("Vous n'avez pas récupéré votre particule.");
         }
-        
+        System.out.println("Votre nouveau solde est :" + getJoueur().getSolde());
+        getFini();
+    }
+    
+    
+    public Jeu(int taille){
+        setFini(false);
+        setTab(LesCases.getTab(10));
+        Scanner input=new Scanner(System.in);
+        System.out.println("Entrez votre pseudo :");
+        setJoueur(new Joueur(input.nextLine()));
+        }
+
+    @Override
+    public String toString() {
+        return "Jeu{" + "fini=" + fini + ", joueur=" + joueur + ", tab=" + tab + '}';
+    }
+    public Direction getDirInitiale(Position p){
+        Direction dir=new Direction(0,0);
+        int taille = getTab().getCases().length;
+        if (p.getX()==0 && p.getY()<taille && p.getY()>=0)
+            dir=new Direction(1, 0);
+        if (p.getX()==(taille-1) && p.getY()<taille && p.getY()>=0)
+            dir=new Direction(-1, 0);
+        if (p.getY()==0 && p.getX()<taille && p.getX()>=0)
+            dir=new Direction(0, 1);
+        if (p.getY()==(taille-1) && p.getX()<taille && p.getX()>=0)
+            dir=new Direction(0, -1);
+        return dir;
+    }
+    public void avance(){
+        Position pos=getJoueur().getParticule().getPos();
+        Direction dir=getJoueur().getParticule().getDir();
+        getTab().getCases()[pos.getX()][pos.getY()].setContenu(getJoueur().getParticule());
+        getTab().getCases()[pos.getX()][pos.getY()].action();
+        pos.setX(pos.getX()+dir.getX());
+        pos.setY(pos.getY()+dir.getY());
+    }
 }
